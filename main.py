c17649668fd5a1e241d8bff5a4172eecad0de8d4
alex <drofa528@bk.ru>
2023-02-22 15:13:20 +0500

    home 2

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 import os
 import json
-
 from googleapiclient.discovery import build
 
-class Channel:
+class Channel():
 
-    def __init__(self, channel_id):
-        self.channel_id = channel_id
-        # API_KEY скопирован из гугла и вставлен в переменные окружения
+    def __init__(self, id):
+        self.__id = id
+        self.name = Channel.print_info(self)['items'][0]['snippet']['title']
+        self.description = Channel.print_info(self)['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.__id
+        self.subscriber_count = Channel.print_info(self)['items'][0]['statistics']['subscriberCount']
+        self.video_count = Channel.print_info(self)['items'][0]['statistics']['videoCount']
+        self.view_count = Channel.print_info(self)['items'][0]['statistics']['viewCount']
+
+    @property
+    def id(self):
+        return self.__id
+
+    @classmethod
+    def get_service(cls):
         api_key: str = os.getenv('API_KEY')
-        # создать специальный объект для работы с API
         youtube = build('youtube', 'v3', developerKey=api_key)
-        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        return youtube
 
 
     def print_info(self):
-        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+        channel = Channel.get_service().channels().list(id=self.__id, part='snippet,statistics').execute()
+        return channel
 
-    # channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+    def to_json(self, file_name):
+        channel_dict = self.__dict__
+        print(type(channel_dict))
+        with open(f'{file_name}', 'w', encoding='windows-1251') as file:
+            channel = json.dumps(channel_dict, indent=2, ensure_ascii=False)
+            file.write(channel)
 
-youtube = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-youtube.print_info()
+    def __str__(self):
+        return f'Youtube-канал: {self.name}'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+id1 = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
+id2 = 'UC1eFXmJNkjITxPFWTy6RsWg'
+ch1 = Channel(id1)
+ch2 = Channel(id2)
+print(ch1)
+print(ch2)
+print(ch1 + ch2)
+print(ch1 < ch2)
+
+
+
 
