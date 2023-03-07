4cc3f0d2354ef65678239f80cf2437fcc4b4cde0
alex <drofa528@bk.ru>
2023-03-07 20:35:39 +0500

    4 домашка

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ class Channel():
 
     @classmethod
     def get_service(cls):
-        api_key: str = os.getenv('API_KEY')
+        api_key: str = os.getenv('YT_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
 
     def print_info(self):
-        channel = Channel.get_service().channels().list(id=self.__id, part='snippet,statistics').execute()
+        channel = Channel.get_service().channels().list(id=self.__id, part='snippet').execute()
         return channel
 
     def to_json(self, file_name):
@@ -44,15 +44,68 @@ class Channel():
     def __lt__(self, other):
         return self.subscriber_count > other.subscriber_count
 
-id1 = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
-id2 = 'UC1eFXmJNkjITxPFWTy6RsWg'
-ch1 = Channel(id1)
-ch2 = Channel(id2)
-print(ch1)
-print(ch2)
-print(ch1 + ch2)
-print(ch1 < ch2)
+
+class Video():
+
+    def __init__(self, id):
+        self.id = id
+
+    @property
+    def title(self):
+        video_info = Video.get_service().videos().list(id=self.id, part="snippet").execute()
+        return video_info['items'][0]['snippet']['title']
+
+    @property
+    def likes(self):
+        video_info = Video.get_service().videos().list(id=self.id, part="statistics").execute()
+        return video_info['items'][0]['statistics']['likeCount']
+
+    @property
+    def views(self):
+        video_info = Video.get_service().videos().list(id=self.id, part="statistics").execute()
+        return video_info['items'][0]['statistics']['viewCount']
+
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def __str__(self):
+        return f'{self.title}'
+
+class PLVideo(Video):
+    def __init__(self, id, pl_id):
+        super().__init__(id)
+        self.pl_id = pl_id
+
+    @property
+    def pl_title(self):
+        playlist = PLVideo.get_service().playlists().list(id=self.pl_id, part='snippet').execute()
+        return playlist['items'][0]['snippet']['title']
+
+    def __str__(self):
+        return f'{self.title} ({self.pl_title})'
 
 
 
 
+
+
+# video = Video('BBotskuyw_M')
+# print(video)
+# print(video.likes)
+# print(video.views)
+
+# id = 'PLPLtdcj9QWBvFSWQmCLwrjA3uL39zmiE6'
+# api_key: str = os.getenv('YT_KEY')
+# youtube = build('youtube', 'v3', developerKey=api_key)
+# video_info = youtube.playlistItems().list(playlistId=id, part="contentDetails", maxResults = 50).execute()
+# print(video_info)
+# playlist_id = 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD'
+# playlist = youtube.playlists().list(id=playlist_id, part='snippet').execute()
+# playlist_name = playlist['items'][0]['snippet']['title']
+# print(playlist)
+
+print(PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD'))
